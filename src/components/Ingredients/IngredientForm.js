aec9ea4c8a7a8ce714e0a360afa228f9e51ca6b3
ngirimana <chadrackngirimana@gmail.com>
2020-04-30 12:05:31 +0200

    Array Destructuring

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({ title: '', amount: '' })
+  const [inputState,setInputState ]= useState({ title: '', amount: '' })
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -16,20 +16,20 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={ submitHandler }>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={ inputState[ 0 ].title }
+            <input type="text" id="title" value={ inputState.title }
               onChange={ event => {
                 const newTitle = event.target.value
-                inputState[ 1 ]((prevInputState) => ({
+                setInputState((prevInputState) => ({
                   title: newTitle, amount: prevInputState.amount
                 }))
               } } />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={ inputState[ 0 ].amount }
+            <input type="number" id="amount" value={ inputState.amount }
               onChange={ event => {
                 const newAmount = event.target.value
-                inputState[ 1 ](prevInputState => ({
+                setInputState(prevInputState => ({
                   title: prevInputState.title, amount: newAmount
                 }))
               } } />
