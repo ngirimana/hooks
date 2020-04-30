ee0a435ed2e70caba6beb0024bc07b26c09ed36c
ngirimana <chadrackngirimana@gmail.com>
2020-04-30 12:16:34 +0200

    Multiple States

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [inputState,setInputState ]= useState({ title: '', amount: '' })
+  const [ enteredTitle, setEnteredTitle ] = useState('')
+  const [ enteredAmount, setEnteredAmount ] = useState('')
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -16,22 +17,16 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={ submitHandler }>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={ inputState.title }
+            <input type="text" id="title" value={ enteredTitle }
               onChange={ event => {
-                const newTitle = event.target.value
-                setInputState((prevInputState) => ({
-                  title: newTitle, amount: prevInputState.amount
-                }))
+                setEnteredTitle(event.target.value)
               } } />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={ inputState.amount }
+            <input type="number" id="amount" value={ setEnteredAmount }
               onChange={ event => {
-                const newAmount = event.target.value
-                setInputState(prevInputState => ({
-                  title: prevInputState.title, amount: newAmount
-                }))
+                setEnteredAmount(event.target.value)
               } } />
           </div>
           <div className="ingredient-form__actions">
