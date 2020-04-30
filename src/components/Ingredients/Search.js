2a7ebd3c4104bf39adf7de79c9b37fc006a8f20d
ngirimana <chadrackngirimana@gmail.com>
2020-04-30 16:00:22 +0200

    Working with Refs & useRef()

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,8 +6,11 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props
   const [ enteredFilter, setEnteredFilter ] = useState('');
+  const inputRef=useRef();
   useEffect(() => {
-    const query=enteredFilter.length===0?'':
+    setTimeout(()=>{
+      if(enteredFilter===inputRef.current.value){
+      const query=enteredFilter.length===0?'':
     `?orderBy="title"&equalTo="${enteredFilter}"`
     fetch('https://react-hooks-update-b527f.firebaseio.com/ingredients.json'+query)
       .then(response => {
@@ -23,13 +26,18 @@ const Search = React.memo(props => {
         }
         onLoadIngredients(loadedIngredients)
       });
-  }, [ enteredFilter, onLoadIngredients ])
+    }},500)
+    
+  }, [ enteredFilter, onLoadIngredients ,inputRef])
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={ enteredFilter } onChange={ event => setEnteredFilter(event.target.value) } />
+          <input 
+          ref={inputRef}
+          type="text" value={ enteredFilter }
+           onChange={ event => setEnteredFilter(event.target.value) } />
         </div>
       </Card>
     </section>
