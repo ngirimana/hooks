ab02f6cb4e1832700ee2865e1370215899f01485
ngirimana <chadrackngirimana@gmail.com>
2020-04-30 14:46:42 +0200

    useEffect() & Loading Data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
-
+  const [ userIngredients, setUserIngredients ] = useState([]);
+  useEffect(() => {
+    fetch('https://react-hooks-update-b527f.firebaseio.com/ingredients.json')
+      .then(response => {
+        return response.json();
+      }).then(responseData => {
+        const loadedIngredients = []
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id:key,
+            title:responseData[key].title,
+            amount:responseData[key].amount
+          })
+        }
+        setUserIngredients(loadedIngredients)
+      })
+  },[])
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-b527f.firebaseio.com/ingredients.json', {
       method: 'POST',
@@ -32,13 +47,13 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <IngredientForm onAddIngredient={ addIngredientHandler } />
 
       <section>
         <Search />
         <IngredientList
-          ingredients={userIngredients}
-          onRemoveItem={removeIngredientHandler}
+          ingredients={ userIngredients }
+          onRemoveItem={ removeIngredientHandler }
         />
       </section>
     </div>
